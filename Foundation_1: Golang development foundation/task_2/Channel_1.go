7db62d7f53ce6c9d4095614057c903bb025c0ce6
Zhen-in-progress <110034078+Zhen-in-progress@users.noreply.github.com>
2025-10-24 15:41:54 +1100

    go channel task

---
+++ Foundation_1: Golang development foundation/task_2/Channel_1.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	ch := make(chan int, 10)
+	go sendTen(ch)
+	go receiveTen(ch)
+	time.Sleep(1000 * time.Millisecond)
+
+}
+
+func sendTen(ch chan int) {
+	for num := 1; num <= 10; num++ {
+		ch <- num
+	}
+	close(ch)
+}
+
+func receiveTen(ch chan int) {
+	for num := range ch {
+		fmt.Println(num)
+	}
+}
